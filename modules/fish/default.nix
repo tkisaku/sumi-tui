4a34670f78df3c9d9d3fdc81365dd3a7a26d658e
T-Kisaku <kisakutakashi0226@gmail.com>
2026-07-18 09:33:45 +0900

    remove helix mode

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -1,42 +1,13 @@
 {
-  pkgs,
-  lib,
-  ...
-}:
-let
-  fish-helix = pkgs.stdenv.mkDerivation {
-    pname = "fish-helix";
-    version = "git-2025-09-24";
-    src = pkgs.fetchFromGitHub {
-      owner = "sshilovsky";
-      repo = "fish-helix";
-      rev = "main";
-      hash = "sha256-04cL9/m5v0/5dkqz0tEqurOY+5sDjCB5mMKvqgpV4vM=";
-    };
-    installPhase = ''
-      mkdir -p $out/share/fish-helix/functions
-      cp -r functions/* $out/share/fish-helix/functions/
-    '';
-  };
-in
-{
   imports = [
     ./plugins.nix
     ./aliases.nix
   ];
 
   programs.fish.enable = true;
-  programs.fish = {
-    interactiveShellInit = ''
-      fish_helix_key_bindings
-    '';
-  };
-  xdg.configFile."fish/functions".source = "${fish-helix}/share/fish-helix/functions";
 
   programs.zoxide = {
     enable = true;
     enableFishIntegration = true;
   };
-  programs.fzf.enableFishIntegration = true;
-
 }
